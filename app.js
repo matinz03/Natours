ff5b719cb63a92a1e11bb6beda3d56fd9ab42314
Matin Zomorrodabedi <s307223@studenti.polito.it>
2025-09-24 12:04:48 +0200

    chore: setup project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+const app = express();
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`);
+});
